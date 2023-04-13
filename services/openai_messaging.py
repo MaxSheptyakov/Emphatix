86a4417e0e2d39aa2b45fec36609a3fb2c5cbad7
Maksim Sheptyakov <maksim.sheptyakov@jetbrains.com>
2023-04-13 19:15:27 +0200

    OPenai messaging

--- services/openai_messaging.py
+++ services/openai_messaging.py
@@ -1,13 +1,18 @@
 from .openai_api import generate_openai_result_async
 
 
-async def get_response_to_emotion(emotion, intensity):
+async def get_response_to_emotion(emotion, intensity, trigger_first, trigger_second):
     messages = [
             {"role": "user",
              "content": """Я хочу, чтобы ты вел себя как чуткий, поддерживающий и добрый друг. """
-                        """Я представлю тебе эмоцию и ее интенсивность по шкале от 1 до 10. Ты должен ответить только """
+                        """Я напишу тебе свою эмоцию и ее интенсивность по шкале от 1 до 10, и может быть триггер, """
+                        """который вызвал эту эмоцию. Ты должен ответить только """
                         """фразой поддержки, советом или ободрением в зависимости от ситуации, и ничем другим. """
-                        """Не задавай уточняющих вопросов. Ты должен говорить по-человечески и не использовать """
-                        f"""слово "уровень". Мой запрос: эмоция {emotion}, интенсивность {intensity}."""},
+                        """Не задавай уточняющих вопросов, не предлагай поговорить об этом"""
+                        """. Ты должен говорить по-человечески и не использовать """
+                        f"""слово "уровень". Мой запрос: эмоция {emotion}, интенсивность {intensity} """+
+                        (f""", триггер {trigger_first}, а точнее {trigger_second}.""" if trigger_first is not None
+                                                                                         and trigger_second is not None
+                         else '.')},
         ]
     return await generate_openai_result_async(messages)
